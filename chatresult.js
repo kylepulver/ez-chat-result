1bcd4e53c34f6c6ce2f08672e535f3200f7b1898
kp <pulverk@gmail.com>
2024-08-23 17:07:49 -0700

    commenting out console logs and stuff

--- chatresult.js
+++ chatresult.js
@@ -19,13 +19,13 @@ const expandResult = (key) => {
     return resultsList[key];
 }
 
-Hooks.on("ready", () => {
-    console.log("EZ Chat Result | whats up");
-})
+// Hooks.on("ready", () => {
+//     console.log("EZ Chat Result | whats up");
+// })
 
 Hooks.on("renderChatLog",  (application, html, data) => {
     html.on("click","li .ez-chat-result .controls button", (ev) => {
-        console.log(ev)
+        // console.log(ev)
         const result = ev.target.dataset.result;
         const message = (getMessage(ev.target.closest("li")));
         if (!resultsList.hasOwnProperty(result)) {
@@ -37,10 +37,10 @@ Hooks.on("renderChatLog",  (application, html, data) => {
     })
 });
 
-Hooks.on("getChatLogEntryContext", (application, entryOptions) => {
-    console.log("EZ Chat Result | Chat Log Entry Context");
-    console.log(application);
-    console.log(entryOptions);
+// Hooks.on("getChatLogEntryContext", (application, entryOptions) => {
+//     console.log("EZ Chat Result | Chat Log Entry Context");
+//     console.log(application);
+//     console.log(entryOptions);
     // entryOptions.push({
     //     name: "Test <em data-aaa='hi'>WHAT</em>",
     //     condition: (li) => {
@@ -52,16 +52,19 @@ Hooks.on("getChatLogEntryContext", (application, entryOptions) => {
     //         console.log(message);
     //     }
     // })
-})
+// })
 
 Hooks.on("renderChatMessage", (application, html, data) => {
     const message = getMessage(html);
-    console.log(message, data)
+    // console.log(message, data)
 
     const result = message.getFlag("ez-chat-result", "result")
 
     const showResults = !!result;
     const showControls = isGm() && !showResults;
+
+    // isn't there a better way to do this?
+    // ya prolly but i dont care lmao
 
     if (showResults) {
         if (isGm()) {
